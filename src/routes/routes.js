24a5b964da2350ba308f9538bf1ec19f4ebdf69f
Luis Alberto <l.al98@hotmail.com>
2023-10-30 23:57:52 -0600

    second commit

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,10 +3,14 @@ import { router as usersRouter } from '../modules/users/users.route.js'
 import { router as restaurantRouter } from '../modules/restaurants/restaurant.route.js'
 import { protect } from '../modules/users/auth.middleware.js'
 import { router as mealsRouter } from '../modules/meals/meals.route.js'
+import { router as orderRouter } from '../modules/orders/orders.route.js'
+
 
 export const router = express.Router()
 
 router.use('/users', usersRouter)
-router.use('/meals', mealsRouter)
+
 router.use(protect)
 router.use('/restaurants', restaurantRouter)
+router.use('/meals', mealsRouter)
+router.use('/orders', orderRouter)
